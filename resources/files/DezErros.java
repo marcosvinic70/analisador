86a4c34b921c9d09d3b30ed615444594473a1ffb
Marcos <marcos.oliveira@gt4w.com.br>
2018-11-04 22:26:43 -0200

    arquivo com 10 erros de sintaxe

---
+++ resources/files/DezErros.java
@@ -0,0 +1,105 @@
+import java.util.Scanner; //entrada e saída de dados
+
+public class DezErros {
+
+	private void parenthesis() {
+
+		if(true {
+
+			return;
+
+		}
+
+	}
+
+	private void operator() {
+
+		int resultado;
+		int i = 10;
+		int j = 5;
+
+		resultado = i  j;
+
+	}
+
+	private void semicolon() {
+
+		int j = 5
+
+	}
+
+	private void bracket() {
+
+		int v[ = new int[n];
+
+	}
+
+	private void reservedWord() {
+
+		boolean a = while false;
+
+	}
+
+	private void doWhileSyntax() {
+
+		float raio, v;
+		raio = 0;
+
+		do {
+
+			v = 4/3 * (float) 3.14 * (float) Math.pow(raio, 3);
+
+			System.out.println("Para raio: " + raio + " o volume é: " + v);
+
+			raio = raio + 0.5f;
+
+		};
+
+	}
+
+	private void forSyntax() {
+
+		float raio, v;
+		raio = 0;
+
+		for(int i = 0) {
+
+			v = 4/3 * (float) 3.14 * (float) Math.pow(raio, 3);
+
+			System.out.println("Para raio: " + raio + " o volume é: " + v);
+
+			raio = raio + 0.5f;
+
+		}
+
+	}
+
+	private void whileSyntax() {
+
+		float raio, v;
+		raio = 0;
+
+		while() {
+
+			v = 4/3 * (float) 3.14 * (float) Math.pow(raio, 3);
+
+			System.out.println("Para raio: " + raio + " o volume é: " + v);
+
+			raio = raio + 0.5f;
+
+		}
+
+	}
+
+
+	( // parenteses fora de algum contexto
+
+	private void brace() {
+
+		if(true) {
+
+			return;
+
+	}
+
+}
